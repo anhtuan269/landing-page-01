7dfa4c449a56a18d585b6e33b9eb83c123a379ff
zeroo <anhtuan090795@gmail.com>
2021-04-22 13:21:50 +0700

    feat(add) add axios package and import Style.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,12 +11,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+     { href:"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap", rel:"stylesheet" }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/tailwind.css',
+    '@/assets/css/Style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
